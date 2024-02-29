f4b2d358f6405edba8af583b76c46fee1261ca24
Amresh Singh <amresh.igniterpro@gmail.com>
2024-02-29 13:39:37 +0530

    feature/set_layout

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,26 @@
+<header class="header">
+    <div class="header__wrapper">
+
+   
+    <div class="header__logo-box">
+        <a href="#" class="header__logo"><img src="{{asset('images/home/logo.png')}}" alt="Logo" >
+        </a>
+        
+    </div>
+    <div class="header__menu">
+        <ul class="header__menu-list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Blogs</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div class="header__hamburger" style="display: none">
+        <li class="line"></li>
+        <li class="line"></li>
+        <li class="line"></li>
+    </div>
+     </div>
+</header>
